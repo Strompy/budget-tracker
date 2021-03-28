9afbe76d49223835cf769092ae4075ed98aa5b22
Phillip Strom <phillip.strom@gmail.com>
2021-03-27 18:22:31 -0600

    Add context to Budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-  return (
-    <div className='alert alert-secondary'>
-      <span>Budget: $2,000</span>
-    </div>
-  );
+	const { budget } = useContext(AppContext);
+
+	return (
+		<div className='alert alert-secondary'>
+			<span>Budget: ${budget}</span>
+		</div>
+	);
 };
 
 export default Budget;
