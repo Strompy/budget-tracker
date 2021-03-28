7e52052c53e76193b2e61a5cda928187a2f7bd5e
Phillip Strom <phillip.strom@gmail.com>
2021-03-27 18:22:08 -0600

    Create AppContext and AppProvider

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,10 +1,35 @@
+import React, { createContext, useReducer } from 'react';
+
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
 const initialState = {
   budget: 2000,
   expense: [
-    { id: 12, name: 'shopping' cost: 40 },
-    { id: 13, name: 'holiday' cost: 400 },
-    { id: 14, name: 'car service' cost: 50 }
+    { id: 12, name: 'shopping', cost: 40 },
+    { id: 13, name: 'holiday', cost: 400 },
+    { id: 14, name: 'car service', cost: 50 }
   ]
 };
 
-export const AppContext = CreateContext();
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+	const [state, dispatch] = useReducer(AppReducer, initialState);
+
+	return (
+		<AppContext.Provider
+			value={{
+				budget: state.budget,
+				expenses: state.expenses,
+				dispatch,
+			}}
+		>
+			{props.children}
+		</AppContext.Provider>
+	);
+};
