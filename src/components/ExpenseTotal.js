9522b23b9104e3f9929824feae94845683c1da98
Phillip Strom <phillip.strom@gmail.com>
2021-03-29 15:41:51 -0600

    Add total expense calculation

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
-const ExpenseTotal = () => {
-  return (
-    <div className='alert alert-primary'>
-      <span>Spent so far: $1,000</span>
-    </div>
-  );
+const Remaining = () => {
+	const { expenses, budget } = useContext(AppContext);
+
+	const totalExpenses = expenses.reduce((total, item) => {
+		return (total = total + item.cost);
+	}, 0);
+
+	const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
+	return (
+		<div className={`alert ${alertType}`}>
+			<span>Remaining: ${budget - totalExpenses}</span>
+		</div>
+	);
 };
 
-export default ExpenseTotal;
+export default Remaining;
